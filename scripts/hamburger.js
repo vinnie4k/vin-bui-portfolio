602d333689b8c2f59c105b7f0faf7c0c33d91f1d
Vinhan Bui <vdb23@cornell.edu>
2023-01-16 12:36:27 -0600

    Fresh start

--- scripts/hamburger.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.hamburger').click(function() {
-    $('.hamburger').toggleClass("active");
-    $('.nav-list').toggleClass("active");
-    // $('main').toggleClass("darken");
-    $('body').toggleClass("fixed-position");
-  })
-})
